fb8f6754ec5ff0adb35535e69cf12b68eb393cd7
Guru Rahul <gururahul123456789@gmail.com>
2024-08-15 20:43:37 +0530

    first common divisor

--- Python/first_number.py
+++ Python/first_number.py
@@ -1,4 +1,21 @@
-number = int(input("Enter the number :"))
+'''number = int(input("Enter the number :"))
 string =str(number)
 first_number =int(string[0])
-print(first_number)
+print(first_number)'''
+
+def input_field():
+    number = int (input("Enter the number :"))
+    return logical_operation(number)
+def logical_operation(number):
+    divisor=[]
+    for i in range(2,number):
+        if number%i==0:
+            divisor.append(i)
+            break
+        else :
+            print(f"It is a Prime number it will divisible by 1 and {number} itself ")
+    return output_field(divisor,number)
+def output_field(divisor,number):
+    print(f"The first common divisor for the number {number} is :",divisor[0])
+
+input_field()
